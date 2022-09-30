3ac379f67993932ac066370d2bbf1df170118eea
Greg Nelson <gln@georgetown.edu>
2022-09-29 23:43:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UM COS420 with React Hooks and TypeScript
             </header>
             <hr></hr>
             <Counter></Counter>
