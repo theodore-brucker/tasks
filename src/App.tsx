2f69d329ef521e25ebaffeb44809b1cbdcf4b628
theodore-brucker <76602421+theodore-brucker@users.noreply.github.com>
2022-10-20 15:38:31 -0400

    Completed HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <header className="App-header">Theo COS420 Brucker</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+                <div style={{ background: "blue" }}>
+                    <h1>This is header text</h1>
+                </div>
+                ;
+                <img
+                    src="../assets/images/pet-ada.jpg"
+                    alt="A picture of my dog Ada"
+                />
             </p>
+
+            <ol>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ol>
         </div>
     );
 }
