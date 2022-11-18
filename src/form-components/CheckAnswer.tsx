cb4175cae381c56139428bb89a22e41c4fd6f1e4
theodore-brucker <76602421+theodore-brucker@users.noreply.github.com>
2022-11-17 20:19:04 -0500

    Completed the checkAnswer component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    // This is the State (Model)
+    const [answer, setAnswer] = useState<string>("");
+
+    // This is the Control
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formMovieName">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+            <div>The movie is {answer === expectedAnswer ? "✔️" : "❌"}.</div>
         </div>
     );
 }
